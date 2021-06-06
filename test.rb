4a47ac13faa9763f37dd2b3c8434c06fbe5e1648
supertramp1978 <yagidaisuke2978@gmail.com>
2021-06-06 15:40:36 +0900

    modified test code

--- test.rb
+++ test.rb
@@ -1,61 +1,81 @@
 require 'spec_helper'
-
+　#gitがインストールされているか
 describe package('git'), :if => os[:family] == 'amazon' do
   it { should be_installed }
 end
 
+#nginxがインストールされているか
 describe package('nginx'), :if => os[:family] == 'amazon' do
   it { should be_installed }
 end
 
-describe package('mysql'), :if => os[:family] == 'amazon' do
+#rubyがインストールされているか
+describe package('ruby'), :if => os[:family] == 'amazon' do
   it { should be_installed }
 end
 
+#railsがインストールされているか
+describe package('rails'), :if => os[:family] == 'amazon' do
+  it { should be_installed }
+end
+
+#railsがインストールされているか
+describe package('yarn'), :if => os[:family] == 'amazon' do
+  it { should be_installed }
+end
+
+#mariadbがインストールされていないか
+describe package('mariadb'), :if => os[:family] == 'amazon' do
+  it { should be_installed }
+end
+
+#mysqlがインストールされているか
+describe package('mysql-community-client'), :if => os[:family] == 'amazon' do
+  it { should be_installed }
+end
+
+#nginxが起動しているか
 describe service('nginx'), :if => os[:family] == 'amazon' do
   it { should be_enabled }
   it { should be_running }
 end
 
-describe service('puma'), :if => os[:family] == 'amazon' do
-  it { should be_enabled }
-  it { should be_running }
-end
-
-describe service('org.apache.httpd'), :if => os[:family] == 'darwin' do
-  it { should be_enabled }
-  it { should be_running }
-end
-
+　#22番ポートが空いているか
 describe port(22) do
   it { should be_listening }
 end
 
+　#80番ポートが空いているか
 describe port(80) do
   it { should be_listening }
 end
 
+　#3000番ポートが空いているか
 describe port(3000) do
   it { should be_listening }
 end
 
+ #パブリックルートテーブルのルーティングをテストしたいのですが、記述方法がわかりませんでした。
 describe routing_table do
   it do
     should have_entry(
-      :destination => '192.168.100.0/24',
-      :interface   => 'eth1',
-      :gateway     => '192.168.10.1',
+      :destination => '0.0.0.0/0', # ここは設定通り
+      :interface   => 'eth1',　　#ここもよくわからず...
+      :gateway     => '',　　　#インターネットゲートウェイのプライベートIP？
     )
   end
 end
 
 
-describe 'MySQL config parameters' do
-  context mysql_config('innodb-buffer-pool-size') do
-    its(:value) { should > 100000000 }
-  end
-
-  context mysql_config('socket') do
-    its(:value) { should eq '/tmp/mysql.sock' }
-  end
+#niginxの設定確認
+describe file('/etc/nginx/conf.d/sample_app.conf') do
+  it { should exist }
+   its(:content) { should match server_name <EC2のPublic IP> }
 end
+
+#niginxの設定確認
+describe file('config/puma.rb') do
+  it { should exist }
+   its(:content) { should match bind "unix://#{Rails.root}/tmp/sockets/puma.sock" }
+end
+
